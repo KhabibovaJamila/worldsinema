8207b209d9d847633e5ab15fef3d096bb0d7a4ab
Jamila <rammv4117@gmail.com>
2025-01-07 22:46:11 +0300

    Добавил Splash Screen

--- app/src/main/java/com/example/world_sinema/MainActivity.kt
+++ app/src/main/java/com/example/world_sinema/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.world_sinema
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+
+
+private val SplashScreen:Long = 2500
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this,AuthorizationActivity::class.java))
+            finish()
+        }, SplashScreen)
     }
 }
